312ae667c09e6285982156349baf0cad79af539a
hi101000 <60079731+hi101000@users.noreply.github.com>
2020-09-18 20:29:01 -0400

    added comments

--- mathhelp/__init__.py
+++ mathhelp/__init__.py
@@ -1,28 +1,35 @@
 import math
 
 def TR(n):
+	'''function to gve you triangular number n'''
 	tr=n*(n-1)
 	return tr
 def tn(n, t1, d):
+	'''gives you term n in an arithmatic sequence'''
 	tn=t1+d*(n-1)
 	return tn
 
+#define some constants
 pi=3.14159265358979323846264338327950
 e=2.7182818284590452353602874713527
 
 def sqrt(n):
+	'''give square root of n'''
 	sqrty=math.sqrt(n)
 	return sqrty
 
 def square(n):
+	'''return the square of n'''
 	n=n**2
 	return n
 
 def pow(x, y):
+	'''return x raised to the power y'''
 	n=x**y
 	return n
 
 def pythagorean(a, b):
+	'''return c from a and b, for pythagorean theorem'''
 	a**2+b**2=c2
 	c=sqrt(c2)
 	return c
